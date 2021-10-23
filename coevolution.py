d0f5e8b1c7918bdbb74083446c94447c75b2daf9
David Franz <davidfranznz@gmail.com>
2021-10-23 18:55:03 +1300

    refactored

--- coevolution.py
+++ coevolution.py
@@ -9,7 +9,7 @@ f1 = lambda x: (1/x) + math.sin(x)
 f2 = lambda x: (2*x) + (x**2) + 3
 f = lambda x: f1(x) if x > 0 else f2(x)
 
-GPR.run(f1)
+GPR.run(f2)
 
 # two individual threads that both look to a solution
 # then we evaluate here
